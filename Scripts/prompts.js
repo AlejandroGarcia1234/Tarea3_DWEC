d7488cc325ee4ded38ea72647bf62d7d579614be
Alejandro Garcia <jandro_es@hotmail.com>
2023-11-16 22:03:31 +0100

    Retoques finales

--- Scripts/prompts.js
+++ Scripts/prompts.js
@@ -1,17 +1,17 @@
 export function obtenerNombre() {
 
     let nombre;
-    let esTexto = false;
+    let valido = false;
   
-    while (!esTexto) {
+    while (!valido) {
       // Solicita al usuario que escriba su nombre
       nombre = prompt("Escriba su nombre:");
       // Verifica si el nombre contiene solo letras
-      esTexto = /^[A-Za-z]+$/i.test(nombre);
+      valido = /^[A-Za-z]+$/i.test(nombre);
       // Si no es un texto válido, solicita al usuario que lo escriba nuevamente
-      if (!esTexto) {
+      if (!valido) {
         nombre = prompt("Vuelva a escribirlo. Su nombre solo puede contener letras:");
-        esTexto = /^[A-Za-z]+$/i.test(nombre);
+        valido = /^[A-Za-z]+$/i.test(nombre);
       }
     }
   
@@ -22,17 +22,17 @@ export function obtenerNombre() {
   export function obtenerApellidos() {
     
     let apellidos;
-    let esTexto = false;
+    let valido = false;
   
-    while (!esTexto) {
+    while (!valido) {
       // Solicita al usuario que escriba sus apellidos
       apellidos = prompt("Escriba sus apellidos:");
       // Verifica si los apellidos contienen solo letras y espacios
-      esTexto = /[a-zA-ZÀ-ÖØ-öø-ÿ\s]+/i.test(apellidos);
+      valido = /[a-zA-ZÀ-ÖØ-öø-ÿ\s]+/i.test(apellidos);
       // Si no son un texto válido, solicita al usuario que los escriba nuevamente
-      if (!esTexto) {
+      if (!valido) {
         apellidos = prompt("Vuelva a escribirlos. Sus apellidos solo pueden contener letras:");
-        esTexto = /[a-zA-ZÀ-ÖØ-öø-ÿ\s]+/i.test(apellidos);
+        valido = /[a-zA-ZÀ-ÖØ-öø-ÿ\s]+/i.test(apellidos);
       }
     }
   
@@ -43,17 +43,17 @@ export function obtenerNombre() {
   export function obtenerFechaNacimiento() {
 
     let fecha;
-    let esFecha = false;
+    let valido = false;
      
-    while (!esFecha) {
+    while (!valido) {
       // Solicita al usuario que escriba su fecha de nacimiento en formato DD/MM/AAAA
       fecha = prompt("Escriba su fecha de nacimiento con formato DD/MM/AAAA:");
       // Verifica si la fecha sigue el formato correcto
-      esFecha = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|[1][0-2])\/[0-9]+$/i.test(fecha);
+      valido = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|[1][0-2])\/[0-9]+$/i.test(fecha);
       // Si no es una fecha válida, solicita al usuario que la escriba nuevamente
-      if (!esFecha) {
+      if (!valido) {
         fecha = prompt("Vuelva a escribir la fecha. Respete el formato DD/MM/AAAA:");
-        esFecha = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|[1][0-2])\/[0-9]+$/i.test(fecha);
+        valido = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|[1][0-2])\/[0-9]+$/i.test(fecha);
       }
     }
   
